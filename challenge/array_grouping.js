f7c2a94666f99f346e19a7982913416e30b5e1f4
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-10-01 21:55:40 +0700

    challenge grouping array

---
+++ challenge/array_grouping.js
@@ -0,0 +1,19 @@
+let items = [
+    { name: 'coca cola', type: 'drink' },
+    { name: 'nasi padang', type: 'food' },
+    { name: 'aqua', type: 'drink' },
+    { name: 'karedok', type: 'food' },
+    { name: 'ayam bakar', type: 'food' },
+    { name: 'pocari sweat', type: 'drink' },
+]
+
+let output  = {
+
+}
+for (let i = 0; i < items.length; i++) {
+    if (output[items[i].type] === undefined) {
+        output[items[i].type] = []
+    }
+    output[items[i].type].push(items[i].name)
+}
+console.log(output)
