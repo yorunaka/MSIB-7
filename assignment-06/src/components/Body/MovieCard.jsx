b0c440c14d2d09943b4e28cf2ba36675799c988a
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-11-28 12:45:54 +0700

    assignment 06 done

--- assignment-06/src/components/Body/MovieCard.jsx
+++ assignment-06/src/components/Body/MovieCard.jsx
@@ -11,7 +11,7 @@ const MovieCard = (props) => {
                 onError={(event) => {
                   event.target.src = "https://images.unsplash.com/photo-1594322436404-5a0526db4d13?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8RXJyb3J8ZW58MHx8MHx8fDA%3D"
                 }} 
-                className="movie-poster" />
+                className="movie-poster bg-cover" />
             </div>
             <div id='title' className='text-center font-semibold text-lg text-white bg-slate-700 p-4'>
                 <h1>{movie.title}</h1>
