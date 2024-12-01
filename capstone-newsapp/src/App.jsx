612ce1b52d8058e3a3acff73e2671d32263d68f1
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-12-01 21:52:41 +0700

    update capstone

---
+++ capstone-newsapp/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <section className="bg-orange-100/75 min-h-screen top-0">
+      {/* navbar */}
+      <section className="min-w-full h-52 bg-red-100 grid grid-flow-row grid-cols-3 place-items-center items-center drop-shadow-lg ">
+        <div>
+          <input type="text" name="" id="" placeholder="Search..." className="px-8 py-2" />
+        </div>
+        <div>
+          <h1>Central Indo News</h1>
+        </div>
+        <div>
+          Harusnya ini logo or, idk
+        </div>
+      </section>
+      
+      {/* body */}
+
+    </section>
+  )
+}
+
+export default App
