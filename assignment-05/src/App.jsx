9cded4270227e1b834d120e499364b20c9beab78
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-12-13 20:40:22 +0700

    update brutall assignment 05

--- assignment-05/src/App.jsx
+++ assignment-05/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [currency, setCurrency] = useState([])
 
   useEffect(() => {
-    axios.get("https://api.currencyfreaks.com/v2.0/rates/latest",{
+    axios.get("https://api.currencyfreaks.com/v2.0/rates/latest?",{
       params: {
         'apikey': `${import.meta.env.VITE_APIKEY}`, //Your API KEY
         'base': 'USD',
