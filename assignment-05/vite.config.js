c2434510b0ff15f5d6287d245c7237d053c73963
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-12-13 21:01:48 +0700

    update config assignment 05

--- assignment-05/vite.config.js
+++ assignment-05/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-    VITE_APIKEY: process.env.VITE_APIKEY,
-  }
 })
